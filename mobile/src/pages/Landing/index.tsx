0882d558baa9347e0fd4f5845178a33ee2c2264e
Alcsaw <alcsaw@hotmail.com>
2020-08-08 09:19:56 -0300

    Day 4: Adds mobile frontend

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import teachIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
+import styles from './styles';
+
+function Landing() {
+  const navigation = useNavigation();
+
+  function handleNavigateToTeachPage() {
+    navigation.navigate('Teach');
+  };
+
+  function handleNavigateToStudyPages() {
+    navigation.navigate('Study');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo! {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToTeachPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={teachIcon} />
+          <Text style={styles.buttonText}>Lecionar</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 28 conexões já realizadas {' '}
+        <Image source={heartIcon} />
+      </Text>
+    </View>
+  );
+};
+
+export default Landing;
